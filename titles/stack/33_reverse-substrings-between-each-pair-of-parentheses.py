376472ac2bbe38579afe7314b77324a339f1f643
birukbelay <belay.bura@gmail.com>
2022-02-01 16:30:12 +0300

    reverse sub string

--- titles/stack/33_reverse-substrings-between-each-pair-of-parentheses.py
+++ titles/stack/33_reverse-substrings-between-each-pair-of-parentheses.py
@@ -2,20 +2,55 @@
 
 def reverseString(s):
     newS=""
-    print(s)    
+    print("s=",s)    
     for i in range(len(s),0,-1):
-        print(i,s[i-1])
+        # print(i,s[i-1])
         newS=newS+s[i-1]
     return newS
-print(reverseString("abebe"))       
+# print(reverseString("abebe"))  
+def makeItAList(input):
+    listOfStr=[]
+    string=""
+    # making a list of each componetn: "(u(love)i)"-->  ["(", "u","(", "love",")","i" ]
+    for i in input:
+        if i==")":
+            listOfStr.append(string)           
+            listOfStr.append(i)
+            string=""            
+        elif i=="(":
+            if string!="":
+                listOfStr.append(string)
+            listOfStr.append(i)
+            string=""
+        else:
+            string+=i
+    if string!="":
+        listOfStr.append(string)
+    return listOfStr
+# print(makeItAList("a(bcdefghijkl(mno)p)q"))         
+# print(makeItAList("(u(love)i)"))         
 def reverseParentheses(s: str) -> str:
     stack=[]
-    string=""
-    for i in s:
-        if s==")":
-            a=""
+    listOfStr=makeItAList(s)
+    
+    print(listOfStr)
+    for i in listOfStr:
+        if i==")":
+            a=stack.pop()
+            # b=reverseString(a)
+            # stack.pop()
+            # stack.append(b)
+            s=""
             while a !="(":
+                s=a+s
                 a=stack.pop()
-                string+=a
+            stack.append(reverseString(s)) 
+            print("stac=>",stack)         
+                                 
         else:
             stack.append(i)
+    print(stack)
+    return "".join(stack)
+print(reverseParentheses("(u(love)i)") )
+print(reverseParentheses("(ed(et(oc))el)") )
+# print(reverseParentheses("a(bcdefghijkl(mno)p)q") )
