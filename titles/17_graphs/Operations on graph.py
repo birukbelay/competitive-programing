ff0ba5bd99660f52df1daac296ecb1b73be01962
bk <belay.bura@gmail.com>
2023-05-24 23:53:18 +0300

    submitted problem on eolymp

--- titles/17_graphs/Operations on graph.py
+++ titles/17_graphs/Operations on graph.py
@@ -1,7 +1,7 @@
 # https://www.eolymp.com/en/contests/9060/problems/78602
 import sys
 input = sys.stdin.readline
-# from collections import Counter
+from collections import defaultdict
 
 n = int(input())
 k = int(input())
@@ -11,8 +11,8 @@ for  i in range(k):
     arr = [int(a) for a in input().split()]
     if arr[0]==1:
         j, l = d[1], d[2]
-        d[j].append[l]
-        d[l].append[j]
+        d[j]=d[j].append(l)
+        d[l].append(j)
     elif arr[0]==2:
         print(d[arr[1]])
     
